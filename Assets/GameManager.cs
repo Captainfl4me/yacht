31a69731cc6257a71f8300ae9316b3b8729a5ff8
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-02-10 14:17:13 +0000

    Add grabable cup

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,43 +5,50 @@ using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
-    private NetworkManager networkManager;
-    public float table_distance = 1.5f;
+    public NetworkManager networkManager;
+    public float tableDistance = 1.5f;
+    public float spawnHeight = 1f;
+
+    private bool gameStarted = false;
 
     private void Awake() {
-        networkManager = GetComponent<NetworkManager>();
-
-        networkManager.OnClientStarted += NetworkManager_OnClientStarted;
         networkManager.OnClientConnectedCallback += NetworkManager_OnClientConnected;
-    }
-
-    private void NetworkManager_OnClientStarted()
-    {
-        if(!networkManager.IsHost)
-        {
-            Destroy(this);
-        }
     }
 
     private void NetworkManager_OnClientConnected(ulong clientId)
     {
         if (!networkManager.IsHost) return;
-        Debug.Log("New player!");
+
+        if (gameStarted) {
+            Debug.Log("Game already started!");
+            networkManager.DisconnectClient(clientId);
+            return;
+        }
 
         float split_table_angle = 360 / networkManager.ConnectedClientsList.Count;
         GameObject[] players_list = GameObject.FindGameObjectsWithTag("Player");
 
         for (int i = 0; i < players_list.Length; i++)
         {
-            players_list[i].transform.position = new Vector3(
-                Mathf.Cos(split_table_angle * i * Mathf.Deg2Rad) * table_distance,
-                0,
-                Mathf.Sin(split_table_angle * i * Mathf.Deg2Rad) * table_distance
-            );
-            // player.rotation = Quaternion.Euler(0, split_table_angle * i, 0);
+            players_list[i].transform.SetPositionAndRotation(new Vector3(
+                Mathf.Cos(split_table_angle * i * Mathf.Deg2Rad) * tableDistance,
+                spawnHeight,
+                Mathf.Sin(split_table_angle * i * Mathf.Deg2Rad) * tableDistance
+            ), Quaternion.Euler(0, -90.0f + split_table_angle * i, 0));
         }
         Debug.Log("New player!");
+    }
+
+    private void Start() {
+        if(networkManager.IsHost) {
+            gameStarted = true;
+            NextRound();
+        }
+    }
+
+    private void NextRound() {
+        networkManager.StartClient();
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,39 +1,158 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerManager : NetworkBehaviour
 {
+    // Network Variables
     private NetworkVariable<Vector3> _networkPosition = new();
     private NetworkVariable<float> _networkRotation = new(writePerm: NetworkVariableWritePermission.Owner);
 
     private AudioListener audioListener;
     private Camera playerCamera;
 
+    public float mouseSensitivity = 4f;
+
+    [Header("UI Elements")]
+    [SerializeField] Sprite grabableSprite;
+    [SerializeField] Sprite grabSprite;
+    [SerializeField] GameObject aimPrefab;
+    [SerializeField] GameObject iconPrefab;
+
+    [Header("Grabbing settings")]
+    [SerializeField] Transform grabPoint;
+    [SerializeField] float maxGrabDistance = 10f;
+    [SerializeField] float grabForce = 150f;
+    [SerializeField] float grabDamping = 1f;
+    private bool lastGrabErrorInitialized = false;
+    private Vector3 lastGrabError = Vector3.zero;
+
+    private GameObject grabbedObject = null;
+    private Rigidbody grabbedRigidbody = null;
+    private Vector3 grabbedPointRelative;
+
     private void Awake()
     {
         Debug.Log("PlayerManager Awake");
         audioListener = GetComponentInChildren<AudioListener>();
         playerCamera = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        if(IsServer) {
+        if (IsServer)
+        {
             _networkPosition.Value = transform.position;
-        } else {
+        }
+        else
+        {
             transform.position = _networkPosition.Value;
         }
-        if (IsOwner) {
+        if (IsOwner)
+        {
+            float yRotation = transform.eulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+            transform.localRotation = Quaternion.Euler(transform.eulerAngles.x, yRotation, 0f);
+
+            float xRotation = playerCamera.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * mouseSensitivity;
+            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, playerCamera.transform.localEulerAngles.y, 0f);
             _networkRotation.Value = transform.rotation.eulerAngles.y;
         }
-        else {
+        else
+        {
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, _networkRotation.Value, transform.eulerAngles.z);
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (!IsOwner) return;
+
+        if (grabbedObject == null)
+        {
+            Ray ray = playerCamera.ScreenPointToRay(new Vector3(playerCamera.scaledPixelWidth / 2, playerCamera.scaledPixelHeight / 2, 0));
+            int layerMask = 1 << 6; // Layer 6 is the grabable layer
+            if (Physics.Raycast(ray, out RaycastHit hit, maxGrabDistance, layerMask))
+            {
+                Debug.DrawRay(ray.GetPoint(0), ray.direction * hit.distance, Color.green);
+                aimPrefab.SetActive(false);
+                iconPrefab.SetActive(true);
+
+                if (Input.GetMouseButton(0))
+                {
+                    if (GrabItem(hit.collider.gameObject))
+                    {
+                        Debug.Log("Grabbing " + grabbedObject.name);
+                        iconPrefab.GetComponent<UnityEngine.UI.Image>().sprite = grabSprite;
+                        grabbedPointRelative = grabbedObject.transform.InverseTransformPoint(ray.GetPoint(hit.distance));
+                        lastGrabError = Vector3.zero;
+                        lastGrabErrorInitialized = false;
+                    }
+                }
+            }
+            else
+            {
+                Debug.DrawRay(ray.GetPoint(0), ray.direction * maxGrabDistance, Color.white);
+                aimPrefab.SetActive(true);
+                iconPrefab.SetActive(false);
+            }
+        }
+        else
+        {
+            // PD controller
+            Vector3 error = grabPoint.position - grabbedObject.transform.TransformPoint(grabbedPointRelative);
+            Vector3 PForce = grabForce * error;
+            Vector3 DForce = lastGrabErrorInitialized ? grabDamping * (error - lastGrabError) / Time.fixedDeltaTime : Vector3.zero;
+            lastGrabError = error;
+            lastGrabErrorInitialized = true;
+
+            grabbedRigidbody.AddForce(PForce + DForce);
+
+            if (!Input.GetMouseButton(0))
+            {
+                if (DropItem()) 
+                {
+                    Debug.Log("Dropping item");
+                    iconPrefab.GetComponent<UnityEngine.UI.Image>().sprite = grabableSprite;
+                }
+            }
+        }
+    }
+
+    private bool GrabItem(GameObject item)
+    {
+        if (grabbedObject != null) return false;
+
+        grabbedRigidbody = item.GetComponent<Rigidbody>();
+        if (grabbedRigidbody == null) 
+        {
+            grabbedRigidbody = item.GetComponentInParent<Rigidbody>();
+            if (grabbedRigidbody == null) return false;
+        }
+
+        grabbedObject = grabbedRigidbody.gameObject;
+        grabbedRigidbody.useGravity = false;
+        grabbedRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        // grabbedObject.transform.SetParent(grabPoint);
+        return true;
+    }
+    private bool DropItem()
+    {
+        if (grabbedObject == null) return false;
+
+        grabbedRigidbody.useGravity = true;
+        grabbedRigidbody.constraints = RigidbodyConstraints.None;
+        // grabbedObject.transform.SetParent(null);
+        grabbedRigidbody = null;
+        grabbedObject = null;
+        return true;
+    }
+
     public override void OnNetworkSpawn()
     {
-        if(!IsOwner) {
+        if (!IsOwner)
+        {
             audioListener.enabled = false;
             playerCamera.enabled = false;
         }
