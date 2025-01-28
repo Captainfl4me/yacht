75d5293576fda5d7e44612e01397fb691993bf79
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-01-28 16:42:40 +0100

    basic server tokio.rs with Dockerfile (build & run)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::UdpSocket;
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    println!("== Start Yatch Server ==");
+    let sock = UdpSocket::bind("0.0.0.0:8080").await?;
+    println!("LOG: Listening to socket 0.0.0.0:8080");
+    let mut buf = [0; 1024];
+    loop {
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        println!("LOG: {:?} bytes received from {:?}", len, addr);
+
+        let len = sock.send_to(&buf[..len], addr).await?;
+        println!("LOG: {:?} bytes sent", len);
+    }
 }
