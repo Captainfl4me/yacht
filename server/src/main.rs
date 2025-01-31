2fbd29f59fa145727110ccc8434750e8628733f5
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-01-31 14:43:39 +0100

    upgrade UDP socket for TCP

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,39 @@
-use tokio::net::UdpSocket;
-use std::io;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("== Start Yatch Server ==");
-    let sock = UdpSocket::bind("0.0.0.0:8080").await?;
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("LOG: Listening to socket 0.0.0.0:8080");
-    let mut buf = [0; 1024];
-    loop {
-        let (len, addr) = sock.recv_from(&mut buf).await?;
-        println!("LOG: {:?} bytes received from {:?}", len, addr);
 
-        let len = sock.send_to(&buf[..len], addr).await?;
-        println!("LOG: {:?} bytes sent", len);
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("LOG: New socket connection!");
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+                println!("LOG: Recv n={}", n);
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+                println!("LOG: Write back n={}", n);
+            }
+        });
     }
 }
