f729cc85ec36e2a9e523a4c4ee64adf6991193e5
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-03-02 11:37:11 +0100

    proper macroquad setup with ewebsocket & server better echo

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,14 +6,14 @@ use colors::*;
 
 #[macroquad::main("Yatch")]
 async fn main() {
-    let mut nm = network::NetworkManager::new("ws://127.0.0.1:8080/").await;
+    let mut nm = network::NetworkManager::new("ws://127.0.0.1:8080/");
 
     let mut scenes: Vec<Box<dyn scenes::Scene>> = vec![Box::<scenes::MenuScene>::default()];
     let mut current_scene: usize = 0;
     scenes[0].on_enter();
 
     loop {
-        nm.udpate().await;
+        nm.udpate();
         scenes[current_scene].update(&mut nm);
 
         clear_background(BLACK);
--- app/src/network.rs
+++ app/src/network.rs
@@ -1,3 +1,5 @@
+use ewebsock::{WsEvent, WsMessage, WsReceiver, WsSender};
+use macroquad::prelude::{error, info};
 
 pub enum ServerAPICommand {
     Ping,
@@ -8,36 +10,68 @@ pub enum ServerAPICommand {
     KeepDice,
 }
 
+#[derive(PartialEq, Eq)]
 pub enum NetworkManagerState {
     NotInit,
     Connecting,
     Connected,
 }
 
+const MAX_RETRY: u8 = 5;
+
 pub struct NetworkManager<'a> {
     state: NetworkManagerState,
     backend_url: &'a str,
+    ws_sender: Option<WsSender>,
+    ws_receiver: Option<WsReceiver>,
+    error_connection_retry: u8,
 }
 
 impl<'a> NetworkManager<'a> {
-    pub async fn new(backend_url: &'a str) -> Self {
+    pub fn new(backend_url: &'a str) -> Self {
         NetworkManager {
             state: NetworkManagerState::NotInit,
             backend_url,
+            ws_sender: None,
+            ws_receiver: None,
+            error_connection_retry: 0,
         }
     }
 
-    pub async fn udpate(&mut self) {
+    pub fn udpate(&mut self) {
         match self.state {
             NetworkManagerState::NotInit => {
-                self.state = NetworkManagerState::Connecting;
+                if self.error_connection_retry < MAX_RETRY {
+                    if self.error_connection_retry > 0 {
+                        info!("Retrying connection: {}", self.error_connection_retry);
+                    }
+                    let (sender, receiver) =
+                        ewebsock::connect(self.backend_url, ewebsock::Options::default()).unwrap();
+                    self.ws_sender = Some(sender);
+                    self.ws_receiver = Some(receiver);
+                    self.state = NetworkManagerState::Connecting;
+                }
             }
             NetworkManagerState::Connecting => {
-                //if self.ws.get_state() == WebSocketState::Opened {
-                    self.state = NetworkManagerState::Connected;
-                //}
+                if let Some(event) = self.ws_receiver.as_ref().unwrap().try_recv() {
+                    match event {
+                        WsEvent::Opened => self.state = NetworkManagerState::Connected,
+                        WsEvent::Error(error_str) => {
+                            error!("{}", error_str);
+                            self.state = NetworkManagerState::NotInit;
+                            self.error_connection_retry += 1;
+                        }
+                        _ => (),
+                    }
+                }
             }
             NetworkManagerState::Connected => {
+                if let Some(event) = self.ws_receiver.as_ref().unwrap().try_recv() {
+                    match event {
+                        WsEvent::Message(msg) => info!("message!"),
+                        _ => (),
+                    }
+                }
                 // let val = futures_util::future::poll_fn(|ctx| {
                 //     match self.ws.poll_next_unpin(ctx) {
                 //         std::task::Poll::Ready(None) => std::task::Poll::Ready(None),
@@ -54,20 +88,23 @@ impl<'a> NetworkManager<'a> {
     }
 
     pub fn is_connected(&self) -> bool {
-        //self.ws.get_state() == WebSocketState::Opened
-        true
+        self.state == NetworkManagerState::Connected
     }
 
     pub fn send_command(&mut self, command: ServerAPICommand) {
-        let mut data: Vec<u8> = match command {
-            ServerAPICommand::Ping => vec![0u8],
-            ServerAPICommand::CreateRoom => vec![1u8],
-            ServerAPICommand::ListRoom => vec![2u8],
-            ServerAPICommand::JoinRoom => vec![3u8],
-            ServerAPICommand::Roll => vec![4u8],
-            ServerAPICommand::KeepDice => vec![5u8],
-        };
+        if self.is_connected() {
+            if let Some(ws) = self.ws_sender.as_mut() {
+                let data: Vec<u8> = match command {
+                    ServerAPICommand::Ping => vec![0u8],
+                    ServerAPICommand::CreateRoom => vec![1u8],
+                    ServerAPICommand::ListRoom => vec![2u8],
+                    ServerAPICommand::JoinRoom => vec![3u8],
+                    ServerAPICommand::Roll => vec![4u8],
+                    ServerAPICommand::KeepDice => vec![5u8],
+                };
 
-        //self.ws.send_binary(data.as_mut_slice());
+                ws.send(WsMessage::Binary(data));
+            }
+        }
     }
 }
