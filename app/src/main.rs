f37a8b75026f670e221ff784027259973ffc4b15
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-03-09 22:25:54 +0100

    add register logic for player uuid

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,14 +1,23 @@
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*};
+use bevy_persistent::prelude::*;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use std::path::Path;
 
 mod colors;
 mod network;
 mod scenes;
 
+#[derive(Resource, Serialize, Deserialize)]
+struct PlayerData {
+    uuid: Uuid,
+}
+
 fn main() {
     App::new()
         .add_plugins(
             DefaultPlugins
-                .set(bevy::log::LogPlugin { ..default() })
+                .set(LogPlugin { ..default() })
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         present_mode: bevy::window::PresentMode::AutoNoVsync,
@@ -25,5 +34,18 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d);
+
+    let config_dir = dirs::data_local_dir().unwrap_or(Path::new("local").join("data")).join("yatch");
+    commands.insert_resource(
+        Persistent::<PlayerData>::builder()
+            .name("Player data")
+            .format(StorageFormat::TomlPretty)
+            .path(config_dir.join("player_data.toml"))
+            .default(PlayerData {
+                uuid: Uuid::new_v4()
+            })
+            .build()
+            .expect("failed to initialize PlayerData"),
+    )
 }
--- app/src/network.rs
+++ app/src/network.rs
@@ -1,17 +1,19 @@
+use super::PlayerData;
 use aeronet_io::{
     connection::{DisconnectReason, Disconnected},
     Session,
 };
 use aeronet_websocket::client::{ClientConfig, WebSocketClient, WebSocketClientPlugin};
 use bevy::prelude::*;
+use bevy_persistent::Persistent;
 use shared::{ServerAPICommand, ServerAPIResponse};
 use speedy::{Readable, Writable};
 use std::collections::VecDeque;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default, States)]
 pub enum NetworkManagerState {
-    Disconnect,
     #[default]
+    Disconnect,
     Connecting,
     Connected,
 }
@@ -37,7 +39,7 @@ pub fn network_plugin(app: &mut App) {
         .add_systems(Startup, ws_setup)
         .add_systems(
             Update,
-            handle_websocket_events.run_if(in_state(NetworkManagerState::Connected)),
+            handle_websocket_events.run_if(not(in_state(NetworkManagerState::Disconnect))),
         )
         .add_observer(on_connected)
         .add_observer(on_disconnected);
@@ -57,7 +59,7 @@ fn client_config() -> ClientConfig {
     ClientConfig::builder().with_no_cert_validation()
 }
 
-fn ws_setup(mut commands: Commands) {
+fn ws_setup(mut commands: Commands, mut nm_state: ResMut<NextState<NetworkManagerState>>) {
     let target = "ws://127.0.0.1:8080";
     let config = client_config();
 
@@ -67,19 +69,27 @@ fn ws_setup(mut commands: Commands) {
         .queue(WebSocketClient::connect(config, target));
 
     commands.insert_resource(NetworkManager::new());
+    nm_state.set(NetworkManagerState::Connecting);
 }
 
 fn on_connected(
     trigger: Trigger<OnAdd, Session>,
     names: Query<&Name>,
     mut nm_state: ResMut<NextState<NetworkManagerState>>,
+    mut nm: ResMut<NetworkManager>,
+    player_data: Res<bevy_persistent::Persistent<PlayerData>>,
 ) {
     let entity = trigger.entity();
     let name = names
         .get(entity)
         .expect("our session entity should have a name");
     info!("{name} connected");
+
     nm_state.set(NetworkManagerState::Connected);
+    nm.send_queue
+        .push_back(ServerAPICommand::Register(shared::RegisterCommand(
+            player_data.uuid,
+        )));
 }
 
 fn on_disconnected(
@@ -113,20 +123,27 @@ fn on_disconnected(
 fn handle_websocket_events(
     mut sessions: Query<(Entity, &Name, Option<&mut Session>)>,
     mut nm: ResMut<NetworkManager>,
+    mut nm_state: ResMut<NextState<NetworkManagerState>>,
 ) {
     if let (_, _, Some(mut ws_session)) = sessions.single_mut() {
         for packet in ws_session.recv.drain(..) {
             if let Ok(res) = ServerAPIResponse::read_from_buffer(&packet.payload) {
-                nm.read_queue.push_back(res);
+                info!("RCV: {:?}", res);
+                if let ServerAPIResponse::Register(shared::RegisterResponse) = res {
+                    nm_state.set(NetworkManagerState::Connected);
+                } else {
+                    nm.read_queue.push_back(res);
+                }
             } else {
                 error!("Response cannot be parsed!");
             }
         }
 
         while let Some(cmd_to_send) = nm.send_queue.pop_front() {
-            let raw_bytes = cmd_to_send.write_to_vec().unwrap();
-            info!("SEND: {:?}", raw_bytes);
-            ws_session.send.push(raw_bytes.into());
+            info!("SEND: {:?}", cmd_to_send);
+            ws_session
+                .send
+                .push(cmd_to_send.write_to_vec().unwrap().into());
         }
     }
 }
