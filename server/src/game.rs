51ea436c9b59753d9babde0a17fb37eb4385e55a
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-04-07 16:13:14 +0200

    server: test point for join room

--- server/src/game.rs
+++ server/src/game.rs
@@ -291,4 +291,63 @@ mod tests {
             panic!("Response type not matching");
         }
     }
+
+    #[tokio::test]
+    async fn test_join_room() {
+        let game_state = Arc::new(Mutex::new(GameState::new()));
+        let mut uuid: Option<uuid::Uuid> = None;
+
+        let register_uuid = uuid::Uuid::new_v4();
+        let register_cmd = ServerAPICommand::Register(shared::RegisterCommand(register_uuid));
+        let room_name = "MyName".to_string();
+        let test_room_uuid = {
+            let mut gs = game_state.lock().await;
+            let test_room = Room::new(Uuid::new_v4(), room_name.clone());
+            let uuid = test_room.uuid();
+            gs.rooms.insert(test_room.uuid(), test_room);
+
+            uuid
+        };
+
+        let join_room_cmd = ServerAPICommand::JoinRoom(shared::JoinRoomCommand(test_room_uuid));
+
+        let res = handle_request(&join_room_cmd, &game_state, &mut uuid).await;
+        assert_eq!(
+            res,
+            ServerAPIResponse::Error(shared::ErrorResponse::NotRegister)
+        );
+
+        let res = handle_request(&register_cmd, &game_state, &mut uuid).await;
+        assert_eq!(res, ServerAPIResponse::Register(shared::RegisterResponse));
+
+        let res = handle_request(&join_room_cmd, &game_state, &mut uuid).await;
+        assert!(matches!(
+            res,
+            ServerAPIResponse::JoinRoom(shared::JoinRoomResponse(_))
+        ));
+
+        let gs = game_state.lock().await;
+        assert_eq!(gs.rooms.get(&test_room_uuid).unwrap().players.len(), 2);
+
+        if let ServerAPIResponse::JoinRoom(shared::JoinRoomResponse(room_uuid)) = res {
+            let room = gs.rooms.get(&room_uuid).unwrap();
+            assert_eq!(room.uuid(), room_uuid);
+            assert_eq!(*room.header.name, room_name);
+            assert!(room.players.contains(&register_uuid));
+
+            let player = gs.players.get(&register_uuid).unwrap();
+            assert_eq!(player.room, Some(room_uuid));
+        } else {
+            panic!("Response type not matching");
+        }
+
+        // Drop game state Mutex lock
+        std::mem::drop(gs);
+
+        let res = handle_request(&join_room_cmd, &game_state, &mut uuid).await;
+        assert_eq!(
+            res,
+            ServerAPIResponse::Error(shared::ErrorResponse::PlayerAlreadyInRoom)
+        );
+    }
 }
